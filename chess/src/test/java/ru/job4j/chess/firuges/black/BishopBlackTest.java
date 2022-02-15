339a6a05cdbae4e2b90d2feccac070fc96cda1ef
kai-73 <oroman@rambler.ru>
2022-02-16 01:05:24 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -1,6 +1,4 @@
 package ru.job4j.chess.firuges.black;
-import junit.framework.TestCase;
-import org.junit.Assert;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
